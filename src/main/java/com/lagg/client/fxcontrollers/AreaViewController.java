c330fd6d899a9cfd50160b4599d0ab102d0efb6d
starfighter-ix <ec14370@qmul.ac.uk>
2017-03-12 03:17:12 +0000

    Added ability to change into subtopics.

---
+++ src/main/java/com/lagg/client/fxcontrollers/AreaViewController.java
@@ -0,0 +1,54 @@
+package com.lagg.client.fxcontrollers;
+
+import com.jfoenix.controls.JFXButton;
+import com.lagg.client.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import org.pmw.tinylog.Logger;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by sameen on 12/03/2017.
+ */
+public class AreaViewController implements Initializable {
+
+    @FXML
+    JFXButton btnTopLeft;
+    @FXML
+    JFXButton btnTopCentre;
+    @FXML
+    JFXButton btnTopRight;
+
+    @FXML
+    JFXButton btnCentreLeft;
+    @FXML
+    JFXButton btnCentreRight;
+    @FXML
+    JFXButton btnBottomLeft;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void handleBtnTopCentre(ActionEvent event) {
+        Logger.trace("btnTopCentre clicked");
+
+        // change button text to reflect field
+        btnTopLeft.setText("Drawing & Animation");
+        btnTopCentre.setText("Database Development");
+        btnTopRight.setText("Web Development");
+        btnCentreLeft.setText("Programming");
+        btnCentreRight.setText("Computer Science");
+        btnBottomLeft.setText("");
+        btnBottomLeft.setVisible(false);
+
+
+
+    }
+}
