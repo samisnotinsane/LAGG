aed878d09a2ece3a3d3483e90bd5c4b304691df8
starfighter-ix <ec14370@qmul.ac.uk>
2017-03-12 07:55:17 +0000

    Made graph view

--- src/main/java/com/lagg/client/fxcontrollers/AreaViewController.java
+++ src/main/java/com/lagg/client/fxcontrollers/AreaViewController.java
@@ -1,14 +1,22 @@
 package com.lagg.client.fxcontrollers;
 
 import com.jfoenix.controls.JFXButton;
+import com.lagg.Topic;
 import com.lagg.client.Main;
+import com.lagg.graph.TopicGraph;
+import com.lagg.graph.TopicSubgraph;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import org.pmw.tinylog.Logger;
 
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 
 /**
@@ -54,13 +62,41 @@ public class AreaViewController implements Initializable {
     public void handleBtnTopRight(ActionEvent event) {
         Logger.trace("btnTopRight clicked");
         // user clicked on 'web development'
-        // show graph screen.
-        try {
-            Pane graphViewPane = Main.loadGraphViewPane();
-            Main.rootPane.setCenter(null);
-            Main.rootPane.setCenter(graphViewPane);
-        } catch (Exception e) {
-            Logger.error("Failed to load area view {}", e);
+
+        Main.rootPane.setCenter(null);
+
+        // dynamically generate buttons on a fresh pane.
+        VBox graphViewPane = new VBox();
+        graphViewPane.setSpacing(10);
+
+        Main.rootPane.setCenter(graphViewPane);
+        graphViewPane.setPrefHeight(120.0);
+        graphViewPane.setPrefWidth(170.0);
+
+
+        TopicSubgraph topicSubgraph = TopicGraph.getTopicsUniverse().getTopics("Linear algebra");
+        HashSet<Topic> topics = topicSubgraph.findEntryPoints(); // topics in graph
+
+        while (!(topics.isEmpty())) {
+
+            HBox hbox = new HBox(30);
+            hbox.getHgrow(graphViewPane);
+            hbox.setAlignment(Pos.CENTER);
+
+            for (Topic topic : topics) {
+                hbox.getChildren().add(new Button(topic.getName()));
+            }
+            graphViewPane.getChildren().add(hbox);
+
+            HashSet<Topic> temps = new HashSet<>();
+            for (Topic topic : topics) {
+                for(Topic temp : topicSubgraph.findChildren(topic))
+                    if(!topics.contains(temp))
+                        temps.add(temp);
+            }
+            topics = new HashSet<>(temps);
         }
+
+
     }
 }
