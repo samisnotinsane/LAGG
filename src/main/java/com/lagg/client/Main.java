6162b89702581c6e6ad6266a258e9817a4fecbbe
starfighter-ix <ec14370@qmul.ac.uk>
2017-03-12 02:17:26 +0000

    Added area view

--- src/main/java/com/lagg/client/Main.java
+++ src/main/java/com/lagg/client/Main.java
@@ -23,7 +23,25 @@ import java.util.Locale;
 public class Main extends Application {
 
     public static final String ABSOLUTE_PATH_TO_RESOURCES_DIR =
-            "/Users/sameenislam/Documents/Repositories/FacebookHack/LAGG/src/main/resources";
+            "src/main/resources";
+
+    public static final String ABSOLUTE_PATH_TO_MAINLAYOUT =
+            "src/main/resources/MainLayout.fxml";
+
+    public static final String ABSOLUTE_PATH_TO_TOP_NAV_VIEW =
+            "src/main/resources/TopPaneView.fxml";
+
+
+    public static final String ABSOLUTE_PATH_TO_LOGIN =
+            "src/main/resources/Login.fxml";
+
+    public static final String ABSOLUTE_PATH_TO_AREA_VIEW =
+            "src/main/resources/AreaView.fxml";
+
+    public static final String ABSOLUTE_PATH_TO_GRAPH_VIEW =
+            "src/main/resources/GraphView.fxml";
+
+    public static BorderPane rootPane;
 
     public static void main(String[] args) {
         configureLog();
@@ -49,10 +67,10 @@ public class Main extends Application {
             root = FXMLLoader.load( urlToMainLayout );
             loginPane = FXMLLoader.load( urlToLoginPane );
         } catch (Exception e) {
-            Logger.error("Failed to load MainLayout.fxml", e);
+            Logger.error("Failed to load MainLayout.fxml {}", e);
         }
 
-        BorderPane rootPane = (BorderPane)root;
+        rootPane = (BorderPane)root;
         rootPane.setCenter(loginPane);
         
         Scene scene = new Scene(root);
@@ -68,5 +86,19 @@ public class Main extends Application {
                 .formatPattern("[{level}] ({date:yyyy-MM-dd HH:mm:ss}) [{class}.{method}()]: {message}")
                 .level(Level.TRACE)
                 .activate();
+    }
+
+    public static URL loadUrl(String path) throws Exception {
+        return new File(path).toURI().toURL();
+    }
+
+    public static Pane loadAreaViewPane() throws Exception {
+        URL urlToAreaView = loadUrl(ABSOLUTE_PATH_TO_AREA_VIEW);
+        return FXMLLoader.load(urlToAreaView);
+    }
+
+    public static Pane loadTopNavView() throws Exception {
+        URL urlToTopPane = loadUrl(ABSOLUTE_PATH_TO_TOP_NAV_VIEW);
+        return FXMLLoader.load(urlToTopPane);
     }
 }
